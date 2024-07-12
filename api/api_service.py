99d8402ba6f5817e95639ac437bfbb150835265a
pecheritsa <pecheritsa.roman@yandex.ru>
2024-07-12 20:06:40 +0300

    add abc class API, add class HhAPI func get_vacancies

---
+++ api/api_service.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+import requests
+
+
+class API(ABC):
+    @abstractmethod
+    def get_vacancies(self, query: str) -> list:
+        pass
+
+
+class HhAPI(API):
+    """
+Класс для подключения к API hh.ru (https://api.hh.ru/vacancies)
+Параметры GET запроса:
+    Показывать вакансии только с указанеи зарплаты ('only_with_salary': 'true')
+    Регион поиска вакансий Москва ("area": 1)
+    """
+    BASE_URL = "https://api.hh.ru/vacancies"
+
+    def get_vacancies(self, search_query: str) -> list:
+        params = {
+            "text": search_query,
+            "area": 1,
+            "per_page": 100,
+            'only_with_salary': 'true',
+        }
+        response = requests.get(url=self.BASE_URL, params=params)
+        return response.json()['items']
