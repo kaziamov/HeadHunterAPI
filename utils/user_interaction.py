185d9a3c9357a84cd5587d6b0e57d4cda3dff8c2
pecheritsa <pecheritsa.roman@yandex.ru>
2024-07-14 01:46:39 +0300

    some func moved to user_func, project reorganization

--- utils/user_interaction.py
+++ utils/user_interaction.py
@@ -1,7 +1,7 @@
 from api.api_service import HhAPI
 from models.vacancy import Vacancy
 from storage.json_storage import JSONVacancyStorage
-from typing import List
+from utils.user_funcs import filter_vac_salary, sort_vac_for_salary, top_sort_vac, print_vac, overwrite_file
 
 
 def user_interaction() -> None:
@@ -90,53 +90,3 @@ def menu_get_vac_for_salary(json_storage: JSONVacancyStorage) -> None:
         overwrite_file(json_storage, filtered_vacancies)
     else:
         print('Вакансии по данной зарплате не найдены')
-
-
-def filter_vac_salary(vacancies: List[Vacancy], desired_salary: int) -> List[Vacancy]:
-    filtered_vacancies = []
-
-    for vac in vacancies:
-        if vac.salary_from is None and vac.salary_to is None:
-            continue
-
-        if vac.salary_from is not None and vac.salary_to is not None:
-            if vac.salary_from <= desired_salary <= vac.salary_to:
-                filtered_vacancies.append(vac)
-
-        elif vac.salary_from is not None:
-            if vac.salary_from <= desired_salary <= vac.salary_from + 10000:
-                filtered_vacancies.append(vac)
-
-        elif vac.salary_to is not None:
-            if vac.salary_to - 10000 <= desired_salary <= vac.salary_to:
-                filtered_vacancies.append(vac)
-
-    return filtered_vacancies
-
-
-def sort_vac_for_salary(data: List[Vacancy]) -> List[Vacancy]:
-    return sorted(data, reverse=True)
-
-
-def top_sort_vac(data: List[Vacancy], top_n: int) -> List[Vacancy]:
-    return data[:top_n]
-
-
-def print_vac(data: List[Vacancy]) -> None:
-    for vac in data:
-        print(vac)
-    print(f'\nНайдены {len(data)} вакансии\n')
-
-
-def overwrite_file(json_storage: JSONVacancyStorage, data: List[Vacancy]):
-    user_choice = input('Сохранить в файле только полученные вакансии? (Д/Н)  ').lower()
-    if user_choice == 'д':
-        json_storage.add_vacancies(data)
-
-
-
-
-
-
-
-
