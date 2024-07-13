47e6ae5e445c329f2487c02d54ed5a8f465e67da
pecheritsa <pecheritsa.roman@yandex.ru>
2024-07-14 01:45:10 +0300

    add user_func file, project reorganization

---
+++ utils/user_funcs.py
@@ -0,0 +1,45 @@
+from typing import List
+from models.vacancy import Vacancy
+from storage.json_storage import JSONVacancyStorage
+
+
+def filter_vac_salary(vacancies: List[Vacancy], desired_salary: int) -> List[Vacancy]:
+    filtered_vacancies = []
+
+    for vac in vacancies:
+        if vac.salary_from is None and vac.salary_to is None:
+            continue
+
+        if vac.salary_from is not None and vac.salary_to is not None:
+            if vac.salary_from <= desired_salary <= vac.salary_to:
+                filtered_vacancies.append(vac)
+
+        elif vac.salary_from is not None:
+            if vac.salary_from <= desired_salary <= vac.salary_from + 10000:
+                filtered_vacancies.append(vac)
+
+        elif vac.salary_to is not None:
+            if vac.salary_to - 10000 <= desired_salary <= vac.salary_to:
+                filtered_vacancies.append(vac)
+
+    return filtered_vacancies
+
+
+def sort_vac_for_salary(data: List[Vacancy]) -> List[Vacancy]:
+    return sorted(data, reverse=True)
+
+
+def top_sort_vac(data: List[Vacancy], top_n: int) -> List[Vacancy]:
+    return data[:top_n]
+
+
+def print_vac(data: List[Vacancy]) -> None:
+    for vac in data:
+        print(vac)
+    print(f'\nНайдены {len(data)} вакансии\n')
+
+
+def overwrite_file(json_storage: JSONVacancyStorage, data: List[Vacancy]):
+    user_choice = input('Сохранить в файле только полученные вакансии? (Д/Н)  ').lower()
+    if user_choice == 'д':
+        json_storage.add_vacancies(data)
